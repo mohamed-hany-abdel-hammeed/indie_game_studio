466a8ec815c8f78eb467f8ee09ea4df44dcb9b81
Elsayed009 <elsayedelshaerawy@gmail.com>
2026-02-02 20:07:12 +0200

    first push on my own branch of the project  - i updated the server logic

--- indie_game_studio/app.js
+++ indie_game_studio/app.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 8000;
+const mongoose = require('mongoose');
 // const gameRoutes = require('./routes/gameRoutes');
 
 // Middleware
@@ -16,22 +16,41 @@ app.use(express.json());
 //     // useUnifiedTopology: true,
 // })
 const dbHost = process.env.DB_HOST || 8000;
+
 async function connectDB() {
     try {
         await mongoose.connect(dbHost);
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error.message);
-        throw error;
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.log('Failed to connect to MongoDB');
+        console.error('Error details:', err.message);
+        throw err;
     }
 }
 connectDB()
 
-
+const User = require ("./model/user");
 // Basic Route
 app.get('/', (req, res) => {
     res.send('Welcome to the Indie Game Studio API');
 });
 
+app.post('/users', async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        console.log("User created:", user);
+        console.log("Request body:", req.body);
+        res.json({
+            msg: "User created successfully",
+            data: user
+        });
+        res.status(201).json(user);
+        res.send("processe completed");
+    } catch (err) {
+            console.error("Error creating user:", err);
+        res.status(400).json({ error: err.message });
+    }
+});
 // Start Server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
